a3bd1f9e8ab4c8d6c7e240d5bef5d5311619195c
petrosyanNarek <narek.petrosyan017@gmail.com>
2023-03-20 16:53:18 +0400

    set login register

--- src/features/registrationSlice.js
+++ src/features/registrationSlice.js
@@ -1,36 +1,32 @@
-import { createAsyncThunk, createSlice } from "@reduxjs/toolkit"
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import { api } from "../api";
 
 const initialState = {
-    registrValid: true
-}
+  registrValid: true,
+};
 
 export const registration = createAsyncThunk(
-    'user/registration',
-    async function (us, { rejectWithValue }) {
-        try {
-            const response = await api.post("addUser", { ...us });
-            return response.item
-        } catch (err) {
-            rejectWithValue(err)
-        }
+  "user/registration",
+  async function (us, { rejectWithValue }) {
+    try {
+      const response = await api.post("addAdmin", { ...us, verify: true });
+      return response.item;
+    } catch (err) {
+      rejectWithValue(err);
     }
-)
-
+  }
+);
 
 const registrationSlice = createSlice({
-    name: 'registration',
-    initialState,
-    reducers: {
+  name: "registration",
+  initialState,
+  reducers: {},
+  extraReducers: (build) => {
+    build.addCase(registration.fulfilled, (state, action) => {
+      state.registrValid = action.payload;
+    });
+  },
+});
 
-    },
-    extraReducers: (build) => {
-        build
-            .addCase(registration.fulfilled, (state, action) => {
-                state.registrValid = action.payload
-            })
-    }
-})
-
-export const selectRegisterValid = (state) => state.registrValid.registrValid
-export default registrationSlice.reducer
+export const selectRegisterValid = (state) => state.registrValid.registrValid;
+export default registrationSlice.reducer;
--- src/features/userSlice.js
+++ src/features/userSlice.js
@@ -1,74 +1,71 @@
-import { createAsyncThunk, createSlice } from "@reduxjs/toolkit"
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import { api } from "../api";
 
 export const loginUser = createAsyncThunk(
-    'user/loginUser',
-    async function (us, { rejectWithValue }) {
-        const response = await api.post("login", { ...us });
-        const data = await response.data
+  "/loginAdmin",
+  async function (us, { rejectWithValue }) {
+    const response = await api.post("loginAdmin", { ...us });
+    const data = await response.data;
 
-        return data
-
-    }
-)
+    return data;
+  }
+);
 export const logOut = createAsyncThunk(
-    'user/logoutUser',
-    async function (_, { rejectWithValue }) {
-        try {
-            const response = await api.post("logOut");
+  "/logOutAdmin",
+  async function (_, { rejectWithValue }) {
+    try {
+      const response = await api.post("logOutAdmin");
 
-            return response.data
-        } catch (err) {
-            rejectWithValue(err)
-        }
+      return response.data;
+    } catch (err) {
+      rejectWithValue(err);
     }
-)
+  }
+);
 export const getUser = createAsyncThunk(
-    'user/getUser',
-    async function (id, { rejectWithValue }) {
-        try {
-            const response = await api.post("getUser", { id });
+  "/getAdmin",
+  async function (id, { rejectWithValue }) {
+    try {
+      const response = await api.post("getAdmin", { id });
 
-            return response.data.user
-        } catch (err) {
-            return rejectWithValue(err.message)
-        }
+      return response.data.user;
+    } catch (err) {
+      return rejectWithValue(err.message);
     }
-)
+  }
+);
 
 const initialState = {
-    user: {},
-    userError: null,
-    loadingUser: false
-}
-
+  user: {},
+  userError: null,
+  loadingUser: false,
+};
 
 const loginSlice = createSlice({
-    name: 'login',
-    initialState,
-    reducers: {},
-    extraReducers: (build) => {
-        build
-            .addCase(getUser.apply, (state, action) => {
-                state.loadingUser = true
-            })
-            .addCase(getUser.rejected, (state, action) => {
-                state.user = {}
-                state.userError = action.payload
-                state.loadingUser = false
-            })
-            .addCase(getUser.fulfilled, (state, action) => {
-                state.user = action.payload
-                state.loadingUser = false
+  name: "login",
+  initialState,
+  reducers: {},
+  extraReducers: (build) => {
+    build
+      .addCase(getUser.apply, (state, action) => {
+        state.loadingUser = true;
+      })
+      .addCase(getUser.rejected, (state, action) => {
+        state.user = {};
+        state.userError = action.payload;
+        state.loadingUser = false;
+      })
+      .addCase(getUser.fulfilled, (state, action) => {
+        state.user = action.payload;
+        state.loadingUser = false;
+      })
+      .addCase(logOut.fulfilled, (state, action) => {
+        state.user = {};
+      });
+  },
+});
 
-            })
-            .addCase(logOut.fulfilled, (state, action) => {
-                state.user = {}
-            })
-    }
-})
-
-export const selectLoginUser = (state) => state.login.user
-export const selectUserLoading = (state) => state.login.loadingUser
-export const selectUserError = (state) => state.login.userError
-export default loginSlice.reducer
+export const selectLoginUser = (state) => state.login.user;
+export const selectUserLoading = (state) => state.login.loadingUser;
+export const selectUserError = (state) => state.login.userError;
+export default loginSlice.reducer;
